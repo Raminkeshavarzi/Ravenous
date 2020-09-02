c789130ad7218f49791d775cd818b74c80c144d9
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-02 06:12:17 +0200

    imported and added component

---
+++ Ravenous-app/ravenous/src/components/BusinessList/BusinessList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Business.css';
+import Business from '../Business/Business';
+
+
+_ BusinessList _ React.Component {
+    render() {
+        return(
+            <div className="BusinessList">
+                <Business />
+                <Business />
+                <Business />
+                <Business /> 
+                <Business />
+                <Business />
+            </div>
+        );
+    }
+};
+
+export default BusinessList;
