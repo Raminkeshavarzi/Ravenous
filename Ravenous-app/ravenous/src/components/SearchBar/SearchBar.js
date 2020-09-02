b38e86a56216e4c41744ac18c640b069361713a2
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-02 08:12:18 +0200

    fix one minor issue

--- Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
+++ Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 // ************* //
-// This compenet  will be used in the future to search for business
+// This compenet is built component that will be used in the future to search for business
 // ************//
 
 import React from 'react';
@@ -15,34 +15,32 @@ const sortByOptions = {
 // Seacrh Bar component
 
 class SearchBar extends React.Component {
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
 
-//Render Mthood to dynamically create the list items needed to display the sort options (Best Match, Highest Rated, Most Reviewed)  
-renderSortByOptions = () => {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];  
-            return <li key={sortByOptionValue}> {this.sortByOptions} </li>;
-        });
-    }
-
-render() {
+  render() {
     return (
-        <div className="SearchBar">
-            <div className="SearchBar-sort-options">
-                <ul>
-                    {this.renderSortByOptions()}
-                </ul>
-            </div>
-            <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
-            </div>
-            <div className="SearchBar-submit">
-                <a>Let's Go</a>
-            </div>
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
         </div>
-        );
-    }
-};
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
 
