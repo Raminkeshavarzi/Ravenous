76e37845249f2b1fb8cfe1d40bd0009da33538a6
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-02 07:32:44 +0200

    removed function and its codes

--- Ravenous-app/ravenous/src/components/App/App.js
+++ Ravenous-app/ravenous/src/components/App/App.js
@@ -4,12 +4,6 @@ import BusinessList from '../BusinessList/BusinessList';
 import BusinessList from '../SearchBar/SearchBar';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ravenous</h1>
-    </div>
-  );
-}
+
 
 export default App;
