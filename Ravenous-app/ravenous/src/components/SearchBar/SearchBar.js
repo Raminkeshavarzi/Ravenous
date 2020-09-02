20b857c296cb73884890e45597606db2efa742cb
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-02 07:09:08 +0200

    added Object to check api

--- Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
+++ Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
@@ -1,2 +1,9 @@
 import React from 'react';
-import './SearchBar.css'
+import './SearchBar.css';
+
+//object with keys and values that conform to what the API expects to receive 
+const sortByOptions = {
+    "Best Match"    : 'best_match',
+    "Highest Rated" : 'rating',
+    "Most Reviewed" : 'review_count',
+}
