0744d5897da5ef7b269c9d76536c0a3e0daa16f8
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-03 08:48:57 +0200

    added the props to  component

--- Ravenous-app/ravenous/src/components/App/App.js
+++ Ravenous-app/ravenous/src/components/App/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
 			<div className="App">
 				<h1>ravenous</h1>
 				<SearchBar />
-				<BusinessList />
+				<BusinessList business={businesses} />
 			</div>
 		);
 	}
