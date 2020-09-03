777d6b854c099b26e39f3b35f2fc89d74a4bcfe6
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-03 09:46:45 +0200

    added the Footer component

---
+++ Ravenous-app/ravenous/src/components/SearchBar/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer>
+				<di>
+					<h1> Built By RK </h1>
+					<ul>
+						<a href="https://github.com/Raminkeshavarzi"> My GitHub </a>
+						<a href="https://www.linkedin.com/in/keshavarziramin1996/"> Linkedln </a>
+					</ul>
+				</di>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
