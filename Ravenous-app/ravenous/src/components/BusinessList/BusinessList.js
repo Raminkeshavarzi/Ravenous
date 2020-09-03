68955dc787efb8a427edd6d81609f9a14d5361d8
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-03 09:28:59 +0200

    added .map method and render data into App.js

--- Ravenous-app/ravenous/src/components/BusinessList/BusinessList.js
+++ Ravenous-app/ravenous/src/components/BusinessList/BusinessList.js
@@ -2,25 +2,20 @@
 // This compenet is built to component that simulates a list of single businesses
 // ************//
 
-
 import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
-
 class BusinessList extends React.Component {
-    render() {
-        return(
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business /> 
-                <Business />
-                <Business />
-            </div>
-        );
-    }
-};
+	render() {
+		return (
+			<div className="BusinessList">
+				{this.props.businesses.map((business) => {
+					return <Business business={business} />; //=> business is came from business object inside App.js
+				})}
+			</div>
+		);
+	}
+}
 
 export default BusinessList;
