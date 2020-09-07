d8da395d96e84ec1605fc3c00bdbbbb1260422f2
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-07 04:38:44 +0200

    Added the constructor

--- Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
+++ Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
@@ -5,42 +5,48 @@
 import React from 'react';
 import './SearchBar.css';
 
-//object with keys and values that conform to what the API expects to receive 
+//object with keys and values that conform to what the API expects to receive
 const sortByOptions = {
-    "Best Match"    : 'best_match',
-    "Highest Rated" : 'rating',
-    "Most Reviewed" : 'review_count',
-}
+	'Best Match': 'best_match',
+	'Highest Rated': 'rating',
+	'Most Reviewed': 'review_count'
+};
 
 // Seacrh Bar component
 
 class SearchBar extends React.Component {
-  renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
-    });
-  }
+	// constructor
+	constructor(props) {
+		this.state = {
+			term: term,
+			location: location,
+			sortBy: sortBy
+		};
+	}
+	renderSortByOptions() {
+		term;
+		return Object.keys(sortByOptions).map((sortByOption) => {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return <li key={sortByOptionValue}>{sortByOption}</li>;
+		});
+	}
 
-  render() {
-    return (
-      <div className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
-        </div>
-        <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
-        </div>
-        <div className="SearchBar-submit">
-          <a>Let's Go</a>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="SearchBar">
+				<div className="SearchBar-sort-options">
+					<ul>{this.renderSortByOptions()}</ul>
+				</div>
+				<div className="SearchBar-fields">
+					<input placeholder="Search Businesses" />
+					<input placeholder="Where?" />
+				</div>
+				<div className="SearchBar-submit">
+					<a>Let's Go</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
-
