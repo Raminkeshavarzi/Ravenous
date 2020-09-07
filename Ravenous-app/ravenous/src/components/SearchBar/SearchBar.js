b1bfc7a2cd0ff0a7f6c614fc060906588f533635
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-07 05:07:51 +0200

    Added the two new methods

--- Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
+++ Ravenous-app/ravenous/src/components/SearchBar/SearchBar.js
@@ -22,6 +22,8 @@ class SearchBar extends React.Component {
 			location: '',
 			sortBy: 'best_match'
 		};
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
 	}
 
 	getSortByClass(sortByOption) {
