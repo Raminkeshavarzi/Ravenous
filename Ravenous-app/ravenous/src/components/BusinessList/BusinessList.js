755bfd73b08a37dbebf214ed71351092390a8d9b
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-07 14:23:28 +0200

    added the all the changes to the app

--- Ravenous-app/ravenous/src/components/BusinessList/BusinessList.js
+++ Ravenous-app/ravenous/src/components/BusinessList/BusinessList.js
@@ -7,15 +7,17 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
-	render() {
-		return (
-			<div className="BusinessList">
-				{this.props.businesses.map((business) => {
-					return <Business business={business} />; //=> business is came from business object inside App.js
-				})}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} key={business.id} />
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
