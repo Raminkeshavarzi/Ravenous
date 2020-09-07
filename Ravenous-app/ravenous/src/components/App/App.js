9fc8752fbe857930f6750cc48526f072d11c8c3b
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-07 06:11:36 +0200

    fixed the caiptalization

--- Ravenous-app/ravenous/src/components/App/App.js
+++ Ravenous-app/ravenous/src/components/App/App.js
@@ -19,19 +19,19 @@ const business = {
 const businesses = [ business, business, business, business, business, business ];
 
 class App extends React.Component {
-	SearchYelp(term, location, sortBy) {
-		console.log(`Searching Yelp with ${Pizza}, ${Brooklyn}, ${best_match}`);
+  searchYelp(term, location, sortBy) {
+		console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
 	}
-	render() {
-		return (
-			<div className="App">
-				<h1>ravenous</h1>
-				<SearchBar SearchYelp={this.SearchYelp} />
-				<BusinessList businesses={businesses} />
-				<Footer />
-			</div>
-		);
-	}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
