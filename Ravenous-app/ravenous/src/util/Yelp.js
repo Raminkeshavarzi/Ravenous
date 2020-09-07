a2032b00de27487627fcbe88685c7e81d21a9358
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-07 13:47:38 +0200

    added the fetching and async and await functionality

--- Ravenous-app/ravenous/src/util/Yelp.js
+++ Ravenous-app/ravenous/src/util/Yelp.js
@@ -1,1 +1,31 @@
 const apiKey = 'tLfajp8Hn5ifbVQqpT7IOc9IsoUn1YqejgcC0KmfF3_k5m-Y85wAot67rr89jlSvBqJVD0-gDPWSuYLmFaTNqjaEFpnDyV_E-mSahXim7y8zVsQgYtrkRGtvGxdWX3Yx';
+const Yelp = {
+
+    search(term, location, sortBy){
+
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { 
+    headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
